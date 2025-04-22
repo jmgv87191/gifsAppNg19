4bcb5bc79104638676985cea1f79f07f3334c6d9
juan manuel <jmgv87191@gmail.com>
2025-04-22 09:10:23 -0700

    asd

---
+++ src/app/app.routes.ts
@@ -0,0 +1,29 @@
+import { Routes } from '@angular/router';
+            
+export const routes: Routes = [
+    {
+        path:'dashboard',
+        loadComponent: ()=> import( './gifs/pages/dashboard-page/dashboard-page.component' ),
+        children:[
+            {
+                path:'trending',
+                loadComponent: ()=> import( './gifs/pages/trending/trending.component' )
+            },
+            {
+                path:'search',
+                loadComponent: ()=> import( './gifs/pages/search/search.component' )
+            },
+            {
+                path:'**',
+                pathMatch:'full',
+                redirectTo:'trending'
+            }
+        ]
+    },
+
+    {
+        path:'**',
+        pathMatch: 'full',
+        redirectTo:'dashboard'
+    }
+];
---
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption{
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
+
+@Component({
+  selector: 'app-side-menu-options',
+  imports: [RouterLink, RouterLinkActive  ],
+  templateUrl: './side-menu-options.component.html',
+  styleUrl: './side-menu-options.component.css'
+})
+export class SideMenuOptionsComponent {
+
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label:'Trending',
+      subLabel:'Gifs Populares',
+      route:'/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label:'Buscador',
+      subLabel:'Buscar gifs',
+      route:'/dashboard/search'
+    },
+  ]
+
+}
