2bac419a89e5da40fb34faed3196e8cb144526f3
juan manuel <jmgv87191@gmail.com>
2025-04-24 09:11:17 -0700

    asd

---
+++ src/app/gifs/components/gif-list/gif-list-item/gif-list-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, input, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-gif-list-item',
+  imports: [],
+  templateUrl: './gif-list-item.component.html',
+  styleUrl: './gif-list-item.component.css'
+})
+
+
+
+
+export class GifListItemComponent {
+
+  imageUrl = input.required<string>()
+
+}
