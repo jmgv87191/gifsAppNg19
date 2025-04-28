5f3ba2fe073ce321957b0bfce943ce17cdb3fc36
juan manuel <jmgv87191@gmail.com>
2025-04-28 13:16:16 -0700

    asd

--- src/app/gifs/pages/trending/trending.component.ts
+++ src/app/gifs/pages/trending/trending.component.ts
@@ -4,20 +4,6 @@ import { GifsService } from '../../services/gifs.service';
 
 
 
-const imageUrls: string[] = [
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
-];
 
 
 
@@ -29,8 +15,7 @@ const imageUrls: string[] = [
 })
 export default class TrendingComponent {
 
-  gifs = signal(imageUrls);
-
+  
   gifService = inject( GifsService )
-
+  
 }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,6 +13,7 @@ export class GifsService {
   private http = inject(HttpClient);
 
   trendingGifs = signal<Gif[]>([])
+  trendingGifsLoading = signal(true)
 
   loadTrendingGifs(){
     this.http.get<GiphyResponse>(`${ environment.giphyUrl }/gifs/trending`,{
@@ -23,6 +24,8 @@ export class GifsService {
     }).subscribe((resp)=>{
 
       const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data)
+      this.trendingGifs.set(gifs)
+      this.trendingGifsLoading.set(false)
       console.log({gifs})
 
     })
